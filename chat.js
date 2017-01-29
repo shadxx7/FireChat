cbab9d4c22a6516fbfefc4bac5bee5ec45bea22e
shadxx7 <shadabdragon@gmail.com>
2017-01-29 22:35:30 +0530

    Added tons of more functionality and added chat.js,function.js - Modified all files

---
+++ chat.js
@@ -0,0 +1,43 @@
+ifUserIsLoggedIn(function() {
+  console.log(window.currentUser);
+  updateUserData();
+  loadUsers(function(users) {
+    var usersList = "";
+
+    for (var uid in users) {
+      var user = users[uid];
+
+      if (window.currentUser.id !=uid){
+        usersList += renderUser(user);
+      }
+
+    }
+
+    getElement("member").innerHTML = usersList;
+
+  });
+  onClickMultiple("member",function(element){
+    var chat_id = element.id;
+    loadMessages(chat_id, function(messages) {
+      var messagesList = "";
+
+      for (var uid in messages) {
+        var message = messages[uid];
+        messagesList += renderMessage(message);
+      }
+      getElement("messages").innerHTML = messagesList;
+
+    });
+
+    getElement("chat-id").value = chat_id;
+
+  });
+
+  click("send-button",function(){
+    var text = getElement("message-text").value;
+    var chat_id = getElement("chat-id").value;
+    sendMessage(chat_id, text);
+  });
+
+
+});
